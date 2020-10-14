34829e27c3838a23f586434e6b619ad5c3782b05
Alex <alex.grig.sur@gmail.com>
2020-10-14 13:32:30 +0300

    Add files via upload
    Проект на летнюю практику. Рассчитать заранее заданный диффур методом Эйлера

---
+++ summerPracticeEuler/summerPracticeEuler/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace summerPracticeEuler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDifference = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxStep = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(552, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Решение дифференциального уравнения точно и методом ломанных Эйлера";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 196);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(924, 548);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title2.Name = "Title1";
+            title2.Text = "Результат";
+            this.chart1.Titles.Add(title2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Уравнение";
+            // 
+            // textBoxDifference
+            // 
+            this.textBoxDifference.Location = new System.Drawing.Point(770, 59);
+            this.textBoxDifference.Name = "textBoxDifference";
+            this.textBoxDifference.ReadOnly = true;
+            this.textBoxDifference.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDifference.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(587, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Максимальная невязка:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(116, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(253, 28);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "2y\'+3ycosx=e^(2x)(2+3cosx)y^-1";
+            // 
+            // textBoxStep
+            // 
+            this.textBoxStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxStep.Location = new System.Drawing.Point(116, 63);
+            this.textBoxStep.Name = "textBoxStep";
+            this.textBoxStep.Size = new System.Drawing.Size(106, 24);
+            this.textBoxStep.TabIndex = 8;
+            this.textBoxStep.TextChanged += new System.EventHandler(this.textBoxStep_TextChanged);
+            this.textBoxStep.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStep_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Число шагов";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(924, 97);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 756);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxStep);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDifference);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Summer Practice(Григорян А.С.)";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDifference;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxStep;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ summerPracticeEuler/summerPracticeEuler/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Math;
+namespace summerPracticeEuler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        private void Calc()
+        {
+            chart1.Series.Clear();
+            
+            EulerCalc CalcModuleApprox = new EulerCalc(0,1);
+            EulerCalcAccurate CalcModuleAccur = new EulerCalcAccurate(0, 1);
+            
+            double leftBorder = 0;
+            double rightBorder = 1;
+            int N=Convert.ToInt32(textBoxStep.Text);
+            double step = (rightBorder-leftBorder)/N;
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            dataGridView1.Columns.Add("X", "Y\\X");
+            dataGridView1.Rows.Add("Y_Approximate");
+            dataGridView1.Rows.Add("Y_Accurate");
+
+            List<List<double>> resultApprox = CalcModuleApprox.Calc(leftBorder,rightBorder,step);
+            List<List<double>> resultAccur = CalcModuleAccur.Calc(leftBorder, rightBorder, step);
+            Series s1 = new Series("Approximate");
+            Series s2 = new Series("Accurate");
+            s1.ChartType = SeriesChartType.Line;
+            s2.ChartType = SeriesChartType.Line;
+            
+            double maxDifference = 0;
+            
+            for (int i = 0; i < resultApprox.Count; ++i)
+            {
+                dataGridView1.Columns.Add($"X[{i}]", $"{Math.Round(resultApprox[i][0],5)}");
+                dataGridView1.Rows[0].Cells[dataGridView1.Columns.Count-1].Value = Math.Round(resultApprox[i][1],5);
+                dataGridView1.Rows[1].Cells[dataGridView1.Columns.Count - 1].Value = Math.Round(resultAccur[i][1],5);
+
+                s1.Points.AddXY(resultApprox[i][0], resultApprox[i][1]);
+                s2.Points.AddXY(resultAccur[i][0], resultAccur[i][1]);
+                
+                if (Abs(resultApprox[i][1] - resultAccur[i][1]) > maxDifference)
+                    maxDifference = Abs(resultApprox[i][1] - resultAccur[i][1]);
+            }
+            chart1.Series.Add(s1);
+            chart1.Series.Add(s2);
+            textBoxDifference.Text = Math.Round(maxDifference, 5).ToString();
+        }
+        private void textBoxStep_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxStep.TextLength > 0)
+                Calc();
+            else
+                chart1.Series.Clear();
+        }
+
+        private void textBoxStep_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+    class EulerCalc
+    {
+        protected double x;
+        protected double y;
+
+        // 2y'+3ycosx=e^(2x)(2+3cosx)y^-1
+        public EulerCalc(double X, double Y)
+        {
+            x = X;
+            y = Y;
+        }
+        protected virtual double function(double x, double y)
+        {
+            return (Exp(2 * x) * (2 + 3 * Cos(x)) * (1 / y) - 3 * y * Cos(x)) / 2;
+        }
+        public List<List<double>> Calc(double leftBorder, double rightBorder, double step)
+        {
+            List<List<double>> Result = new List<List<double>>();
+            for (double i = leftBorder; i <= rightBorder; i += step)
+            {
+                List<double> XY = new List<double>();
+                XY.Clear();
+                XY.Add(x);
+                XY.Add(y);
+                Result.Add(XY);
+                y += step * function(x, y);
+                x += step;
+            }
+            return Result;
+        }
+    }
+    class EulerCalcAccurate : EulerCalc
+    {
+        public EulerCalcAccurate(double X, double Y) : base(X, Y)
+        {
+        }
+        protected override double function(double x, double y)
+        {
+            return Sqrt(Exp(2 * x));
+        }
+    }
+}
