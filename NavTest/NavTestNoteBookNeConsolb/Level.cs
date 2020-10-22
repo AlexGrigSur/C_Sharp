9ebfb684a19c03c72407dd41e0d31e6dc30ab2c8
Alex <alex.grig.sur@gmail.com>
2020-10-22 18:41:30 +0300

    BeforeTheStorm

--- NavTest/NavTestNoteBookNeConsolb/Level.cs
+++ NavTest/NavTestNoteBookNeConsolb/Level.cs
@@ -36,6 +36,29 @@ namespace NavTest
             floor = Floor;
         }
 
+        public void NodesOptimizer()
+        {
+            int maxX=-1, maxY=-1, minX=Int32.MaxValue, minY= Int32.MaxValue;
+            foreach(List<int> i in nodeListOnFloor.Values)
+            {
+                if (i[0] > maxX) maxX = i[0];
+                if (i[1] > maxY) maxY = i[1];
+                if (i[0] < minX) minX = i[0];
+                if (i[1] < minY) minY = i[1];
+            }
+            if(minX!=10 || minY!=10 )
+                foreach(List<int> i in nodeListOnFloor.Values)
+                {
+                    i[0] -= minX + 10;
+                    i[1] -= minY + 10;
+                }
+            if ((maxX + 10 != screenResX) || (maxY + 10 != screenResY))
+            {
+                screenResX = maxX - minX + 10;
+                screenResY = maxY - minY + 10;
+            }
+        }
+
         #region // поиск вершин
         public List<Node> SearchNode(int x, int y)
         {
@@ -94,7 +117,6 @@ namespace NavTest
         }
         #endregion
     }
-
 
     class Map
     {
