cd6731eaf1b488a09aa6ca0af4bbf7729f9fbe33
Alex <alex.grig.sur@gmail.com>
2020-11-12 13:05:48 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ NavTest/NavTestNoteBookNeConsolb/NavForm/MainNavForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavTestNoteBookNeConsolb.NavForm
+{
+    public partial class MainNavForm : Form
+    {
+        public MainNavForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
