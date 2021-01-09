9e1e144ed10049e671fb3b1ab339af3fb7d0cbc5
Alex <alex.grig.sur@gmail.com>
2021-01-10 01:09:48 +0300

    Basic work on gui

--- NavProject/NavProject-Server/Server.cs
+++ NavProject/NavProject-Server/Server.cs
@@ -27,7 +27,6 @@ namespace NavProject_Server
         public string email { get; set; }
         public bool isConfirmed { get; set; }
     }
-
     class ClientServe
     {
         private int id;
@@ -42,17 +41,72 @@ namespace NavProject_Server
             ServeUser();
         }
 
-        public string CodeGenerator(int length)
+        private string responseString = "";
+        private bool isClientAuthorize = false;
+
+        private string currentMode = "";
+        private string buffer = "";
+
+        public void RecognizeCommand(ref string[] command)
         {
-            Random rand = new Random();
-            string result = "";
-            int value;
-            for (int i = 0; i < length; ++i)
+            switch (command[0])
             {
-                value = rand.Next(0, 36);
-                result += (value < 26) ? Convert.ToChar(value + 65).ToString() : (value - 26).ToString();
+                case "ClientAuthorize":
+                    {
+                        isClientAuthorize = true;
+                        break;
+                    }
+                case "Authorize":
+                    {
+
+                        break;
+                    }
+                case "InsertUser":
+                    {
+
+                        break;
+                    }
+                case "SendConfirmEmailMessage":
+                    {
+                        break;
+                    }
+                case "ConfirmEmail":
+                    {
+                        break;
+                    }
+                case "ForgotPassword":
+                    {
+                        break;
+                    }
+                case "GetPlan": // get plan by id
+                    {
+                        break;
+                    }
+                case "GetPlansList_Public":
+                    {
+                        break;
+                    }
+                case "GetUserPlansList":
+                    {
+                        if (user == null)
+                            Response("Authorization required");
+                        break;
+                    }
+                case "UploadPlan":
+                    {
+                        Response("Authorization required");
+                        break;
+                    }
+                case "Disconnect":
+                    {
+                        break;
+                    }
+                case "TestText":
+                    {
+
+                        break;
+                    }
             }
-            return result;
         }
         void ServeUser()
         {
@@ -61,149 +115,23 @@ namespace NavProject_Server
             byte[] msg;
 
 
-            string responseString = "";
-            bool isClientAuthorize = false;
-
-            string currentMode = "";
-            string buffer = "";
-
             while (client.Connected)
             {
                 msg = new byte[1024];
                 try
                 {
-                    //count = await //msg, 0, msg.Length);//
-                    stream.ReadTimeout = 10000000;
+                    stream.ReadTimeout = 1000;
                     int count = stream.Read(msg, 0, msg.Length);
                     string[] command = Encoding.UTF8.GetString(msg, 0, count).Split();
-
                     Console.WriteLine($"[{id}]Command: {command[0]}");
-                    switch (command[0])
-                    {
-                        case "ClientAuthorize":
-                            {
-                                isClientAuthorize = true;
-                                break;
-                            }
-                        case "Authorize":
-                            {
-                                user = DBInteraction.GetUser(command[1], command[2]);
-                                responseString = (user == null) ? "No matches found" : $"Welcome back,{user.Value.name}";
-                                Response(responseString);
-                                break;
-                            }
-                        case "InsertUser":
-                            {
-                                DBInteraction.InsertUser(command[1], command[2], command[3], command[4]);
-                                user = DBInteraction.GetUser(command[2], command[3]);
-                                Response($"Регистрация успешно пройдена");
-                                break;
-                            }
-                        case "SendConfirmEmailMessage":
-                            {
-                                if (user == null)
-                                {
-                                    Response("Authorization required");
-                                    continue;
-                                }
-                                if(user.Value.isConfirmed)
-                                {
-                                    Response("Email is already confirmed");
-                                    continue;
-                                }
-                                currentMode = "ConfirmEmail";
-                                buffer = CodeGenerator(6);
-                                var smtpClient = new SmtpClient("smtp.gmail.com")
-                                {
-                                    Port = 587,
-                                    UseDefaultCredentials = false,
-                                };
-                                smtpClient.Credentials = new NetworkCredential("navproject.kubsu@gmail.com", "146923785Alex");
-                                smtpClient.EnableSsl = true;
-
-                                var mailMessage = new MailMessage
-                                {
-                                    From = new MailAddress("navproject.kubsu@gmail.com"),
-                                    Subject = "NavProject - регистрация нового пользователя",
-                                    Body = "<p></p>\n" +
-                                    "<h2> NavProject</h2>\n" +
-                                    "<p></p>\n" +
-                                    $"<p> Здравствуйте, {user.Value.name}.</p>\n" +
-                                    "<p> Этот адрес электронной почты был указан для регистрации нового пользователя в самом странном проекте по созданию планов зданий.</p>\n" +
-                                    "<p> Ваш код регистрации:</p>\n" +
-                                    $"<h3><strong>{buffer}</strong><strong></strong></h3>\n" +
-                                    "<p> Учтите, что код действителен в течение 60 минут.</p>\n" +
-                                    "<p></p>\n" +
-                                    "<p> С уважением,</p>\n" +
-                                    "<p> команда NavProject </p>",
-                                    IsBodyHtml = true
-                                };
-                                mailMessage.To.Add(user.Value.email);
-                                smtpClient.Send(mailMessage);
-                                Response("Message was sent");
-                                break;
-                            }
-                        case "ConfirmEmail":
-                            {
-                                if (user == null)
-                                {
-                                    Response("No user signing in");
-                                    continue;
-                                }
-                                if (currentMode != "ConfirmEmail")
-                                {
-                                    Response("No current email confirmation messages");
-                                    continue;
-                                }
-                                if (command[1] == buffer)
-                                {
-                                    user = new User(user.Value.id, user.Value.name, user.Value.login, user.Value.password, user.Value.email, true);
-                                    DBInteraction.ConfirmUser(user.Value.id);
-                                }
-                                else
-                                {
-                                    Response("Wrong code");
-                                    continue;
-                                }
-                                break;
-                            }
-                        case "ForgotPassword":
-                            {
-                                break;
-                            }
-                        case "GetPlan": // get plan by id
-                            {
-                                break;
-                            }
-                        case "GetPlansList_Public":
-                            {
-                                break;
-                            }
-                        case "GetUserPlansList":
-                            {
-                                if (user == null)
-                                    Response("Authorization required");
-                                break;
-                            }
-                        case "UploadPlan":
-                            {
-                                if (user == null)
-                                    Response("Authorization required");
-                                break;
-                            }
-                        case "Disconnect":
-                            {
-                                Console.WriteLine($"[{id}]Client disconnected");
-                                stream.Close();
-                                client.Close();
-                                break;
-                            }
-                        case "TestText":
-                            {
-                                DBInteraction.InsertLotRows(400);
-                                break;
-                            }
-                    }
+                    RecognizeCommand(ref command);
+                }
+                catch (System.IO.IOException)// e)
+                {
+                    Console.WriteLine($"[{id}]Connection Was aborted - RecieveRuntime");
+                    Response("Connection was aborted - RecieveRuntime");
+                    CloseConnection();
+                    return;
                 }
                 catch (Exception e)
                 {
@@ -212,6 +140,7 @@ namespace NavProject_Server
                     CloseConnection();
                     return;
                 }
+
             }
             CloseConnection();
         }
@@ -226,7 +155,6 @@ namespace NavProject_Server
             client.Close();
         }
     }
-
     class Server
     {
         List<TcpClient> clientList;
@@ -249,12 +177,12 @@ namespace NavProject_Server
                     {
                         clientList.Add(server.AcceptTcpClient());
                         Console.WriteLine($"Есть новое подключение, индекс - {clientList.Count}");
-                        //new Thread(() =>
-                        //{
-                        TcpClient client = clientList.Last();
-                        new ClientServe(clientList.Count, client);
-                        clientList.Remove(client);
-                        //}).Start();
+                        new Thread(() =>
+                        {
+                            TcpClient client = clientList.Last();
+                            new ClientServe(clientList.Count, client);
+                            clientList.Remove(client);
+                        }).Start();
                     }
                 }
             }
