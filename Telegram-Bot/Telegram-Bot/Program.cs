e2d05a4e5053a7856b7f81ce14d40d5b021d17b0
Alex <alex.grig.sur@gmail.com>
2021-02-16 02:07:44 +0300

    Telegram bot init

---
+++ Telegram-Bot/Telegram-Bot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram_Bot;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
